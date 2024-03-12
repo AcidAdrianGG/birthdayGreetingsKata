3bed1181646b1dd33716b0069e6c6186025074d3
= <glezgalvan.adrian@gmail.com>
2024-03-12 09:26:51 +0000

    test: displays happy birthday to 1 person

--- src/birthdayGreetings.spec.ts
+++ src/birthdayGreetings.spec.ts
@@ -1,15 +1,40 @@
 import { describe, it, expect } from "vitest"
 import { BirthdayGreetings } from "./birthdayGreetings.js"
+import { Greeting } from "./birthdayGreetings.js"
+import { Person } from "./birthdayGreetings.js"
 
 describe("BirthDayGreetings", () => {
   it("displays error when there is no birthday greetings to send", () => {
-    const birthdayGreetings = new BirthdayGreetings()
-
+    const birthdayGreetings = createBirthdayGreetings(11, 4, 2024, [])  
     try {
-      const greetings: string = birthdayGreetings.sendGreetings()
+      birthdayGreetings.sendGreetings()
     } catch (error) {
       expect(error.message).toBe("no birthdays for today")
     }
+  })
 
+  it("displays happy birthday to 1 person", () => {
+    const person = {
+      name: "John",
+      lastName: "Doe",
+      dateOfBirth: new Date(1982, 9 , 8),
+      email: 'johnDoe@example.com'
+    }
+    const birthdayGreetings: BirthdayGreetings = createBirthdayGreetings(8, 9, 1982, [person])
+
+    const greetings: Greeting[] = birthdayGreetings.sendGreetings()
+
+    expect(greetings.length).toBe(1)
+    expect(greetings[0].subject).toBe("Happy birthday!")
+    expect(greetings[0].body).toBe("Happy birthday, dear " + person.name + '!')
   })
 })
+
+function createBirthdayGreetings(day: number, month: number, year: number, people: Person[]) {
+  const today = new Date("2024-03-11T00:00:00.000Z")
+  today.setDate(day)
+  today.setMonth(month)
+  today.setFullYear(year)
+  const clockStub = { now: () => today }
+  return new BirthdayGreetings(clockStub, people)
+}
--- src/birthdayGreetings.ts
+++ src/birthdayGreetings.ts
@@ -1,7 +1,52 @@
+export interface Greeting {
+  subject: string
+  body: string
+}
+
+export interface Person {
+  name: string
+  lastName: string
+  dateOfBirth: Date
+  email: string
+}
+export class Clock {
+  now() {
+    return new Date()
+  }
+}
+
+
 export class BirthdayGreetings {
-  
-  sendGreetings(): string {
-    throw Error('no birthdays for today')
+  private readonly clock: Clock
+  private readonly people: Person[]
+
+  constructor(clock: Clock, people: Person[] = []) {
+    this.clock = clock
+    this.people = people
   }
 
+  sendGreetings(): Greeting[] {
+    let greetings: Greeting[] = []
+
+    this.people.forEach((person: Person) => {
+      if (this.isThisPersonBirthdayToday(person)) {
+        greetings.push({
+          subject: "Happy birthday!",
+          body: "Happy birthday, dear " + person.name + "!",
+        })
+      }
+    })
+
+    if (greetings.length != 0) {
+      return greetings
+    }
+
+    throw Error("no birthdays for today")
+  }
+
+  private isThisPersonBirthdayToday(person: Person): boolean {
+    const today: Date = this.clock.now()
+    return person.dateOfBirth.getDate() == today.getDate() &&
+      person.dateOfBirth.getMonth() == today.getMonth();
+  }
 }
